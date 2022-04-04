db8a026bddc234a93c89a72250179774fd507f49
Acer <Acer@LAPTOP-3JBUJVV9>
2022-04-04 15:25:12 +0700

    first commit

---
+++ namanyarahma/src/main/java/com/mycompany/namanyarahma/Namanyarahma.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.namanyarahma;
+
+/**
+ *
+ * @author Acer
+ */
+public class Namanyarahma {
+    
+   int personAge;
+
+   public Namanyarahma(String name) {
+      // This constructor has one parameter, name.
+      System.out.println("Name chosen is :" + name );
+   }
+
+   public void setAge( int age ) {
+      personAge = age;
+   }
+
+   public int getAge( ) {
+      System.out.println("Person's age is :" + personAge );
+      return personAge;
+   }
+
+   public static void main(String []args) {
+      
+      Namanyarahma myName = new Namanyarahma( "rahma" );
+      
+      myName.setAge( 19 );
+
+      myName.getAge( );
+          
+      System.out.println("Variable Value :" + myName.personAge );
+   }
+}
