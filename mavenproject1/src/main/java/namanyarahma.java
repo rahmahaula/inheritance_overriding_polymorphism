2425d24d847fd3a72c07f19db0ebfd9102c99968
Acer <Acer@LAPTOP-3JBUJVV9>
2022-04-04 17:37:34 +0700

    first commit

--- mavenproject1/src/main/java/namanyarahma.java
+++ mavenproject1/src/main/java/namanyarahma.java
@@ -7,32 +7,3 @@
  *
  * @author Acer
  */
-public class namanyarahma {
-    
-   int personAge;
-
-   public namanyarahma(String name) {
-      // This constructor has one parameter, name.
-      System.out.println("Name chosen is :" + name );
-   }
-
-   public void setAge( int age ) {
-      personAge = age;
-   }
-
-   public int getAge( ) {
-      System.out.println("Person's age is :" + personAge );
-      return personAge;
-   }
-
-   public static void main(String []args) {
-      
-      namanyarahma myName = new namanyarahma( "rahma" );
-      
-      myName.setAge( 19 );
-
-      myName.getAge( );
-          
-      System.out.println("Variable Value :" + myName.personAge );
-   }
-}
